d82628d7e511f11bcb15af2bef4eeebdaa2d2e0b
Luis <a20190459@pucp.edu.pe>
2023-06-12 12:33:55 -0500

    primero

---
+++ app/src/main/java/com/example/lab5/DoctorModel.java
@@ -0,0 +1,103 @@
+package com.example.lab5;
+
+public class DoctorModel {
+    private String apellido;
+    private String ciudad;
+    private int edad;
+    private String email;
+    private String estado;
+    private String nacionalidad;
+    private String nombre;
+    private String pais;
+    private String telefono;
+    private String usuario;
+    private String imagen;
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+}
---
+++ app/src/main/java/com/example/lab5/MainAdapter.java
@@ -0,0 +1,57 @@
+package com.example.lab5;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+
+public class MainAdapter extends FirebaseRecyclerAdapter<DoctorModel,MainAdapter.myViewHolder> {
+    /**
+     * Initialize a {@link RecyclerView.Adapter} that listens to a Firebase query. See
+     * {@link FirebaseRecyclerOptions} for configuration options.
+     *
+     * @param options
+     */
+    public MainAdapter(@NonNull FirebaseRecyclerOptions<DoctorModel> options) {
+        super(options);
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull myViewHolder holder, int position, @NonNull DoctorModel model) {
+        holder.name.setText(model.getApellido());
+        holder.ciudad.setText(model.getPais());
+
+        Glide.with(holder.img.getContext())
+                .load(model.getImagen()).into(holder.img);
+
+    }
+
+    @NonNull
+    @Override
+    public myViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.main_item,parent,false);
+        return new myViewHolder(view);
+    }
+
+    class myViewHolder extends RecyclerView.ViewHolder{
+        ImageView img;
+        TextView name,ciudad;
+
+        public myViewHolder(@NonNull View itemView){
+            super(itemView);
+            img=(ImageView)itemView.findViewById(R.id.img1);
+            name=(TextView) itemView.findViewById(R.id.nametext);
+            ciudad=(TextView) itemView.findViewById(R.id.ciudad);
+
+        }
+
+    }
+}
